a1f655c62cd5d6860acbcbcd004be1895d280c89
Luke <luke.wangai@student.moringaschool.com>
2025-05-21 16:07:18 +0300

    Bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 3, "M", "Brown", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 6, "M", "White", "Energetic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "Blue", "Relaxed", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Brave", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 4,"F", "White", "Friendly", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M","Green", "Grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F","Burnt orange", "Joyful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 0, "M", "Red", "Kind", NULL);
